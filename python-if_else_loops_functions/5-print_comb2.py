c94ab8ba0a1faec4299799d9ebd13ca2409a068d
root <jecxu@hotmail.com>
2024-05-07 13:51:31 +1000

    Task 5

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+for num in range(0, 100):
+    if num < 10:
+        number = "0{}".format(num)
+    else:
+        number = "{}".format(num)
+
+    if num != 99:
+        print("{}{}{}".format(number, ",", " "), end="")
+    else:
+        print("{}".format(number), end="")
