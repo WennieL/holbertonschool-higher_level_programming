dbc3f73b5de6e6425735c71d2ee70a4ac60f4853
root <jecxu@hotmail.com>
2024-05-08 16:41:14 +1000

    Task 3

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+
+    if len(sys.argv) == 1:
+        print("0")
+    else:
+        for i in range(1, len(sys.argv)):
+            total += int(sys.argv[i])
+        print(total)
