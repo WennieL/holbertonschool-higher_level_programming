3119641479d81cd0e3d1ea3fb1304af37cf1aa5c
root <jecxu@hotmail.com>
2024-05-08 16:06:07 +1000

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import sys
 
-num_args = len(sys.argv)
-i = 1
+if __name__ == "__main__":
+    num_args = len(sys.argv)
 
-if i == 0:
-    print("0 arguments.")
-else:
-    print("{} argument{}:".format(num_args - 1, 's' if num_args > 2 else ''))
-    for j in range(1, num_args):
-        print("{}: {}".format(i, sys.argv[i]))
-        i += 1
+    if num_args == 1:
+        print("0 argument")
+    elif num_args == 2:
+        print("1 argument")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(num_args - 1))
+        for i in range(1, num_args):
+            print("{}: {}".format(i, sys.argv[i]))
