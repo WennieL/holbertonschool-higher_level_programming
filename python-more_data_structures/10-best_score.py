304971fa24d21b557ca1bf0eec0ea45a493aae61
root <jecxu@hotmail.com>
2024-05-11 16:20:56 +1000

    Task 10

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,5 +2,13 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    else:
-        return max(a_dictionary)
+
+    keys_list = list(a_dictionary.keys())
+    k = keys_list[0]
+    max_value = a_dictionary[k]
+
+    for key in keys_list:
+        if a_dictionary[key] > max_value:
+            max_value = a_dictionary[key]
+            k = key
+    return k
