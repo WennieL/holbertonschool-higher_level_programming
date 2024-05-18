8931db176e278b86cd2ea3c24d8ec7c81e8c45fd
root <jecxu@hotmail.com>
2024-05-18 11:19:34 +1000

    update test file

--- python-test_driven_development/2-main.py
+++ python-test_driven_development/2-main.py
@@ -1,10 +1,22 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
+matrix = [[1, 2, 3]]
+print(matrix_divided(matrix))
+
+matrix = [[1, 2, 3], [4, 5, 6]]
+print(matrix_divided(matrix))
+
+matrix = [[1, 2, 3], [4, 5, 6]]
+print(matrix_divided(matrix, float('inf')))
+
 matrix = [[1, 2, 3], [4, 5, 6]]
 print(matrix_divided(matrix, 3))
 
 matrix = [[1, 2, 3], [4, 5, 6]]
+print(matrix_divided(matrix, 2))
+
+matrix = [[1, "ha", 3], [4, 5, 6]]
 print(matrix_divided(matrix, 2))
 
 matrix = [[1.2, 2.6, 3.3], [4.2, 5.7, 6.33]]
@@ -18,3 +30,6 @@ print(matrix_divided(matrix, 3))
 
 matrix = [[1, 2, 3], [4, 5, 6]]
 print(matrix_divided(matrix, 0))
+
+matrix = [[1, "ha", 3], [4, 5, 6]]
+print(matrix_divided(matrix, 2))
--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -25,7 +25,8 @@ def matrix_divided(matrix, div):
     ZeroDivisionError: If div is equal to zero.
     """
 
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+    if not isinstance(matrix, list) or not \
+            all(isinstance(row, list) for row in matrix):
         raise TypeError(
             "matrix must be a matrix (list of lists) of integers/floats")
 
