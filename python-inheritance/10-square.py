6bd9b0753eb8952733260bf67e67509ea4c29d73
root <jecxu@hotmail.com>
2024-05-21 15:55:50 +1000

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 '''
-This moudle has a class Square that  inherits
-frin Rectangle with some methods
+This moudle has a class Square that inherits
+from Rectangle with some methods
 '''
 Rectangle = __import__('9-rectangle').Rectangle
 
@@ -10,9 +10,7 @@ class Square(Rectangle):
     '''Defines a square class that inherits from Rectangle'''
 
     def __init__(self, size):
+        '''Initializes a square with size'''
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
-        self.integer_validator("size", size)
-
-    def __str__(self):
-        return "[Rectangle] {}/{}".format(self.__size, self.__size)
