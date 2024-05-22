32b148f024330da22bc7dd1890a6dc0f5c00a6b4
root <jecxu@hotmail.com>
2024-05-22 12:37:55 +1000

    Task-00

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+This module creates an abstract class named Animal using the
+ABC package.
+'''
+from abc import ABC, abstractmethod
+
+
+class Animal(ABC):
+    '''This defines an abstract class Animal'''
+
+    @abstractmethod
+    def sound(self):
+        '''Abstract method for sound, must be implemented by subclasses'''
+        pass
+
+
+class Dog(Animal):
+    '''This is a subclass of Animal representing a dog'''
+
+    def sound(self):
+        '''the sound method for the Dog class and
+        it returns the string “Bark”.
+        '''
+        return "Bark"
+
+
+class Cat(Animal):
+    '''This is a subclass of Animal representing a cat'''
+
+    def sound(self):
+        '''the sound method for the Cat class and
+        it returns the string “Meow”.
+        '''
+        return "Meow"
