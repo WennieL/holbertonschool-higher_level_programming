0965ea1b7c46b3d03b216d15ea3fe5b49a6ea89b
root <jecxu@hotmail.com>
2024-05-25 11:47:46 +1000

    Task0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,7 +6,6 @@ This module contain a function that reads a text file and print it
 
 def read_file(filename=""):
     '''a function that reads a text file (UTF8) and prints it to stdout:'''
-    with open(filename) as f:
-        if f is None:
-            return
-        print(f.read().rstrip())
+    with open(filename, "r", encoding="utf-8") as f:
+
+        print(f.read(), end="")
