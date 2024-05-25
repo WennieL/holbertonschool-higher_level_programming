74e0e6ba74c4b241e684a9637603085331f30dac
root <jecxu@hotmail.com>
2024-05-25 13:53:16 +1000

    Task 6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+This module contains a function of load_from_json_file
+'''
+
+import json
+
+
+def load_from_json_file(filename):
+    '''a function that creates an Object from a “JSON file”'''
+
+    with open(filename, "r", encoding="utf-8") as f:
+        return json.load(f)
