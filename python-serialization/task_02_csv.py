0dffa2e5a7ff778bb5e785b13025f6faf8300637
root <jecxu@hotmail.com>
2024-05-26 14:51:24 +1000

    Task 2

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -36,8 +36,3 @@ def convert_csv_to_json(csvFile):
     except Exception as e:
         print(f"{e}")
         return False
-
-
-csv_file = "data.csv"
-convert_csv_to_json(csv_file)
-print(f"Data from {csv_file} has been converted to data.json")
