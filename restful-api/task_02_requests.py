04253257cdbaf5fbc0945f074d1a930b4a13cf98
root <jecxu@hotmail.com>
2024-05-30 11:49:18 +1000

    update Task 02

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,17 +7,22 @@ import csv
 
 def fetch_and_print_posts():
     '''fetch  all post from the url'''
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    url = f"https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
     print("Status Code:", response.status_code)
-    posts = response.json()
+
+    # Check if the response is successful
     if response.status_code == 200:
+        posts = response.json()
         for post in posts:
             print(post["title"])
 
 
 def fetch_and_save_posts():
     '''fetch all post from the url'''
-    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    url = f"https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
     if response.status_code == 200:
         posts = response.json()
         with open("posts.csv", "w",  encoding="utf-8", newline="") as csvFile:
