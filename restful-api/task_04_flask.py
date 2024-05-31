66c665944d87a92daac6e68c226f767e65b91c55
root <jecxu@hotmail.com>
2024-05-31 12:30:12 +1000

    Task 04

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -37,41 +37,39 @@ def status():
 
 
 @app.route("/users/<username>")
-def get_user(username):
+def users_specific(username):
     ''' User details endpoint'''
     user = users.get(username)
-    if user:
-        return jsonify(user)
-    else:
+    if not user:
         return jsonify({"error": "User not found"}), 404
+
+    return jsonify(user)
 
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
     '''Add new user'''
-    user_data = request.get_json()
-    if user_data is None:
-        return jsonify({"error": "Not a JSON file"})
+    req_data = request.get_json()
+    if not req_data:
+        return jsonify({"error": "Not a JSON file"}), 400
 
+    username = req_data.get("username")
     # Check if the 'username' field is missing
-    if "username" not in user_data:
+    if not username:
         return jsonify({"error": "Username is required"}), 400
-
-    username = user_data.get("username")
 
     # Check if the username already exists
     if username in users:
         return jsonify({"error": "User already exists"}), 400
 
-    # users[username] = user_data
-    users[username] = {
-        "username": username,
-        "name": user_data.get("name"),
-        "age": user_data.get("age"),
-        "city": user_data.get("city")
-
+    user_data = {
+        "name": req_data.get("name"),
+        "age": req_data.get("age"),
+        "city": req_data.get("city")
     }
-    return jsonify({"message": "User added", "User": users[username]}), 201
+    users[username] = user_data
+
+    return jsonify({"message": "User added", "User": user_data}), 201
 
 
 if __name__ == "__main__":
