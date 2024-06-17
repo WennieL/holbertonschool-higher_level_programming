f5dafb4a99c470c817e9fb12855ed1f69bf710ed
WennieL <jecxu@hotmail.com>
2024-06-18 09:44:07 +1000

    task4_update

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,7 @@
 -- creates a table called first_table in the current database in MySQL server.
 
-CREATE table first_table(
-    id INT,
-    name VARCHAR(256)
-)
+CREATE table if not exists first_table(
+    id INT NOT NULL,
+    name VARCHAR (256) NOT NULL,
+    PRIMARY KEY (id)
+);
