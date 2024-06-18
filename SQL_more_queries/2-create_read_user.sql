8902a9cb6c126668bad4a49a5a8229414c924f0d
WennieL <jecxu@hotmail.com>
2024-06-18 14:15:12 +1000

    task 2

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,4 +4,4 @@ CREATE DATABASE IF not exists hbtn_0d_2;
 CREATE USER IF not exists user_0d_2@localhost
 IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.*
-TO USER user_0d_2@localhost;
+TO user_0d_2@localhost;
