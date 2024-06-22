d2d630a6968b49582191a23e3773fd2f581fe740
WennieL <jecxu@hotmail.com>
2024-06-22 14:18:03 +1000

    task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    usrname = argv[1]
+    pwd = argv[2]
+    db_name = argv[3]
+    state_name = argv[4]
+
+    connect_str = f"mysql+mysqldb://{usrname}:{pwd}@localhost:3306/{db_name}"
+
+    engine = create_engine(connect_str, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_name_search = session.query(
+        State).filter(State.name == state_name).first()
+
+    if state_name_search:
+        print(state_name_search.id)
+    else:
+        print("Not found")
+
+    session.close()
