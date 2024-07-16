a07a559e2e1bced0ec1bdd90e92233a42b793d03
WennieL <jecxu@hotmail.com>
2024-07-16 16:42:54 +1000

    task 7

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,20 @@
+let list_movies = document.getElementById("list_movies");
+
+fetch("https://swapi-api.hbtn.io/api/films/?format=json")
+  .then((response) => {
+    if(!response.ok) {
+      throw new Error('HTTP error: ${response.status');
+    }
+    return response.json();
+  })
+  .then((json) => {
+    const movies = json.results;
+    for (let movie of movies) {
+      let li = document.createElement("li");
+      li.textContent = movie.title;
+      list_movies.appendChild(li);
+    }
+  })
+  .catch((error) => {
+    console.error('Fetch problem: ${error.message');
+  })
