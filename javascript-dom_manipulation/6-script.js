541943a6dbf6a4834373bb0b50ca2f8c39e09afd
WennieL <jecxu@hotmail.com>
2024-07-16 16:16:08 +1000

    task 6

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,13 @@
+let character = document.getElementById("character");
+
+fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+  .then((response) => {
+    if(!response.ok) {
+      throw new Error('HTTP error: ${response.status');
+    }
+    return response.json();
+  })
+  .then((json) => {
+    character.textContent = json.name;
+  })
+  .catch((error) => console.error('Fetch problem: ${error.message'));
