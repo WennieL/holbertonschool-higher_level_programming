ee353a165f982285d36389b049c6527397b18593
WennieL <jecxu@hotmail.com>
2024-07-16 17:08:08 +1000

    task 8

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let hello = document.getElementById("hello");
+
+  fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((json) => {
+      hello.textContent = json.hello;
+    })
+    .catch((error) => console.error('Fetch problem: ${error.message}'));
+});
