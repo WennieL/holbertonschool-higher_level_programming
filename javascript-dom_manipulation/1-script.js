87c250f0dd46e5ff4888cc16b05adfadecd4d5e7
WennieL <jecxu@hotmail.com>
2024-07-16 11:26:44 +1000

    task 1

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,5 @@
+let click = document.getElementById("red_header");
+let title = document.querySelector('header');
+click.addEventListener('click', function() {
+  title.style.color = '#FF0000';
+});
